dabff560ed57251103b7985a6511a7d185c33837
Daniel Huffman <drhuffman12@gmail.com>
2020-03-07 10:09:47 -0500

    add custom bars cond colors

--- spec/ascii_bar_charter_spec.cr
+++ spec/ascii_bar_charter_spec.cr
@@ -241,6 +241,110 @@ describe AsciiBarCharter do
           end
         end
       end
+
+      describe "custom chars and colors" do
+
+        # @bar_chars = new_bar_chars
+        # @bar_colors = new_bar_colors
+
+        bar_chars_custom    = ['\'', '\u2591', '\u2592', '\u2593', '\u2588']
+        # BAR_STEP_QTY = BAR_CHARS.size - 1 # first char is a 'floor', not a 'step'
+        bar_colors_custom   = [:red, :green, :blue, :purple, :orange
+          # Colorize::ColorRGB.new(0, 255, 255),
+          # Colorize::ColorRGB.new(255, 0, 255),
+          # Colorize::ColorRGB.new(255, 255, 0),
+          # Colorize::ColorRGB.new(255, 0, 255),
+          # Colorize::ColorRGB.new(0, 255, 255)
+        ]
+
+
+        describe "prefix enabled" do
+          describe "and reverse disabled" do
+            it "will return color-escaped bars from with prefix numbers ranging from 0.0 to 1.0 in green to red colors" do
+              min = 0.0
+              max = 1.0
+              precision = 2.to_i8
+              in_bw = true
+              data = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+              prefixed = true
+              reversed = false
+              expected_graph_chars = "0.0'0.1'0.2░0.3░0.4▒0.5▒0.6▒0.7▓0.8▓0.9█"
+
+              charter = AsciiBarCharter.new(min, max, precision, in_bw, reversed, bar_chars: bar_chars_custom, bar_colors: bar_colors_custom)
+              plot = charter.plot(data, prefixed)
+
+              puts plot + "\n"
+
+              plot.should eq(expected_graph_chars)
+            end
+          end
+
+          describe "and reverse enabled" do
+            it "it will return color-escaped bars from with prefix numbers ranging from 0.0 to 1.0 in green to red colors" do
+              min = 0.0
+              max = 1.0
+              precision = 2.to_i8
+              in_bw = true
+              data = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+              prefixed = true
+              reversed = true
+              expected_graph_chars = "0.0'0.1'0.2░0.3░0.4▒0.5▒0.6▒0.7▓0.8▓0.9█"
+
+              charter = AsciiBarCharter.new(min, max, precision, in_bw, reversed, bar_chars: bar_chars_custom, bar_colors: bar_colors_custom)
+              plot = charter.plot(data, prefixed)
+
+              puts plot + "\n"
+
+              # Expected: "0.0\e[34;47m_\e[0m0.1\e[32;47m▁\e[0m0.2\e[32;47m▂\e[0m0.3\e[92;47m▃\e[0m0.4\e[92;47m▄\e[0m0.5\e[33;47m▅\e[0m0.6\e[33;47m▆\e[0m0.7\e[91;47m▇\e[0m0.8\e[91;47m█\e[0m0.9\e[31;47m▉\e[0m"
+              # got: "0.0\e[31;47m_\e[0m0.1\e[31;47m▁\e[0m0.2\e[91;47m▂\e[0m0.3\e[91;47m▃\e[0m0.4\e[33;47m▄\e[0m0.5\e[33;47m▅\e[0m0.6\e[92;47m▆\e[0m0.7\e[92;47m▇\e[0m0.8\e[32;47m█\e[0m0.9\e[32;47m▉\e[0m"
+
+              plot.should eq(expected_graph_chars)
+            end
+          end
+        end
+
+        describe "prefix disabled" do
+          describe "and reverse disabled" do
+            it "will return color-escaped bars from without prefix numbers in green to red colors" do
+              min = 0.0
+              max = 1.0
+              precision = 2.to_i8
+              in_bw = true
+              data = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+              prefixed = false
+              reversed = false
+              expected_graph_chars = "''░░▒▒▒▓▓█"
+
+              charter = AsciiBarCharter.new(min, max, precision, in_bw, reversed, bar_chars: bar_chars_custom, bar_colors: bar_colors_custom)
+              plot = charter.plot(data, prefixed)
+
+              puts plot + "\n"
+
+              plot.should eq(expected_graph_chars)
+            end
+          end
+
+          describe "and reverse enabled" do
+            it "will return color-escaped bars from without prefix numbers in green to red colors" do
+              min = 0.0
+              max = 1.0
+              precision = 2.to_i8
+              in_bw = true
+              data = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+              prefixed = false
+              reversed = true
+              expected_graph_chars = "''░░▒▒▒▓▓█"
+
+              charter = AsciiBarCharter.new(min, max, precision, in_bw, reversed, bar_chars: bar_chars_custom, bar_colors: bar_colors_custom)
+              plot = charter.plot(data, prefixed)
+
+              puts plot + "\n"
+
+              plot.should eq(expected_graph_chars)
+            end
+          end
+        end
+      end
     end
   end
 end
--- src/ascii_bar_charter.cr
+++ src/ascii_bar_charter.cr
@@ -2,17 +2,54 @@
 require "colorize"
 
 class AsciiBarCharter
+  class AsciiBarError < Exception end
   VERSION = "1.0.3"
 
   BAR_CHARS    = ['_', '\u2581', '\u2582', '\u2583', '\u2584', '\u2585', '\u2586', '\u2587', '\u2588', '\u2589', '\u258A']
-  BAR_STEP_QTY = BAR_CHARS.size - 1 # first char is a 'floor', not a 'step'
+  # BAR_STEP_QTY = BAR_CHARS.size - 1 # first char is a 'floor', not a 'step'
   BAR_COLORS   = [:blue, :green, :green, :light_green, :light_green, :yellow, :yellow, :light_red, :light_red, :red, :red]
 
-  getter min, max, min_max_delta : Float64, precision, in_bw, inverted
+  getter min, max, min_max_delta : Float64, precision
+  getter in_bw # if true, don't colorize
+  getter inverted
+  getter bar_chars
+  getter bar_colors # : Array(Symbol | Colorize::ColorRGB)
+  getter bar_step_qty : Int32
 
-  def initialize(@min : Float64, @max : Float64, @precision : Int8, @in_bw = false, @inverted = false)
-    @min_max_delta = 1.0 * (max - min)
-    @bar_colors = !in_bw && @inverted ? BAR_COLORS.reverse : BAR_COLORS
+  def initialize(
+    @min : Float64, @max : Float64, @precision : Int8,
+    @in_bw = false, @inverted = false,
+    @bar_chars = BAR_CHARS, @bar_colors = BAR_COLORS
+  )    
+    @min_max_delta = 1.0 * (max - min)    
+    # @bar_colors = @bar_colors.reverse if !in_bw && @inverted
+    @bar_step_qty = @bar_chars.size
+    validate!
+  end
+
+  def validate!
+    raise AsciiBarError.new("Mismatch in sizes of bar_chars and bar_colors") if !in_bw && (bar_chars.size != bar_colors.size)
+  end
+
+  def toggle_bw
+    @in_bw = !in_bw
+    validate!
+  end
+
+  def reset_bars(new_bar_chars = BAR_CHARS, new_bar_colors = BAR_COLORS)
+    @bar_chars = new_bar_chars
+    @bar_colors = new_bar_colors
+    validate!
+  end
+
+  def reset_bar_chars(new_bar_chars = BAR_CHARS)
+    @bar_chars = new_bar_chars
+    validate!
+  end
+
+  def reset_bar_colors(new_bar_colors = BAR_COLORS)
+    @bar_colors = new_bar_colors
+    validate!
   end
 
   def plot(data, prefixed = false)
@@ -22,11 +59,13 @@ class AsciiBarCharter
   end
 
   def bar(single_data, as_bar = true)
-    i = (BAR_STEP_QTY * (single_data - min) / min_max_delta).round.to_i
-    i = 0 if i < 0
-    i = BAR_STEP_QTY if i > BAR_STEP_QTY
-    bar = BAR_CHARS[i].to_s
-    bar = bar.colorize.fore(@bar_colors[i]).back(:light_gray).to_s unless in_bw
+    f = (1.0 * (single_data - min) / min_max_delta)
+    f = 0.0 if f < 0.0
+    f = 1.0 if f > 1.0
+    i = (f * (bar_step_qty - 1)).round.to_i
+    # i = bar_step_qty - 1 if i >= bar_step_qty
+    bar = @bar_chars[i].to_s
+    bar = bar.colorize.fore(@bar_colors[inverted ? (@bar_step_qty - i - 1) : i]).back(:light_gray).to_s unless in_bw
     as_bar ? bar : (single_data.round(precision).to_s + bar.to_s)
   end
 
